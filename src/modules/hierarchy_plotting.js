9beb2d8bd2af2f108b563deba43ba1e2f0100ab7
mcgo0092 <mcgo0092@umn.edu>
2020-06-01 12:24:17 -0500

    add true phylogram to boot trees.

--- src/modules/hierarchy_plotting.js
+++ src/modules/hierarchy_plotting.js
@@ -1,16 +1,8 @@
-import { hierarchy, cluster } from "d3-hierarchy";
-import { create, select } from "d3-selection";
-import { ascending } from "d3-array";
 import { removeChildNodes, cleanExistingPlot, htmlToElement } from "./html_templates";
 
-let event_buld_fn = undefined;
+let event_build_fn = undefined;
 
 const FILE_NAMES = [RegExp(/[Cc]onsensus Tree/), RegExp(/.*boot.*tree.*/)];
-
-const d3 = Object.assign(
-    {},
-    { hierarchy, cluster, create, select, ascending }
-);
 
 // https://github.com/jasondavies/newick.js
 const parseNewick = function (s) {
@@ -48,96 +40,19 @@ const parseNewick = function (s) {
     return tree;
 };
 
-const autoBox = function () {
-    document.body.appendChild(this);
-    const {
-        x,
-        y,
-        width,
-        height
-    } = this.getBBox();
-    document.body.removeChild(this);
-    return [x, y, width, height];
-}
-
-const chart_phylogram = function (obj) {
-    const { parsed_data, dom_id } = obj;
-    const root = d3.hierarchy(parsed_data, d => d.branchset)
-        .sum(d => d.branchset ? 0 : 1)
-        .sort((a, b) => (a.value - b.value) || d3.ascending(a.data.length, b.data.length));
-    root.dx = 10;
-    root.dy = 75;
-    d3.cluster().nodeSize([root.dx, root.dy])(root);
-
-    const svg = d3.create("svg");
-
-    svg.append("g")
-        .attr("fill", "none")
-        .attr("stroke", "#555")
-        .attr("stroke-opacity", 0.4)
-        .attr("stroke-width", 1.5)
-        .selectAll("path")
-        .data(root.links())
-        .join("path")
-        .attr("d", d => `
-        M${d.target.y},${d.target.x}
-        C${d.source.y + root.dy / 2},${d.target.x}
-         ${d.source.y + root.dy / 2},${d.source.x}
-         ${d.source.y},${d.source.x}
-      `);
-
-    svg.append("g")
-        .selectAll("circle")
-        .data(root.descendants())
-        .join("circle")
-        .attr("cx", d => d.y)
-        .attr("cy", d => d.x)
-        .attr("fill", d => d.children ? "#555" : "#999")
-        .attr("r", 2.5);
-
-    svg.append("g")
-        .attr("font-size", 8)
-        .selectAll("text")
-        .data(root.descendants())
-        .join("text")
-        .filter(d => d.data.name.length === 0)
-        .attr("x", d => d.y)
-        .attr("y", d => {
-            return (d.x - 10);
-        })
-        .attr("dy", "0.31em")
-        .filter(d => d.data.length != undefined)
-        .text(d => d.data.length.toPrecision(4));
-
-    svg.append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-width", 3)
-        .selectAll("text")
-        .data(root.descendants())
-        .join("text")
-        .attr("x", d => d.y)
-        .attr("y", d => d.x)
-        .attr("dy", "0.31em")
-        .attr("dx", d => d.children ? -6 : 6)
-        .filter(function (d) {
-            return d.data.name.length != 0;
-        })
-        //.text(d => d.data.name + " : " + d.data.length.toPrecision(3))
-        .text(d => d.data.name)
-        .filter(d => d.children)
-        .attr("text-anchor", "end")
-        .clone(true).lower()
-        .attr("stroke", "white");
-    document.getElementById(dom_id).append(svg.attr("viewBox", autoBox).node());
-}
 
 const animate = function (data) {
+
     let tree_num = 0;
     let parsed_branchset = parseNewick(data[tree_num][0]);
     cleanExistingPlot();
-    chart_phylogram({ parsed_data: parsed_branchset, dom_id: "plot" });
+    dispatchEvent(event_build_fn("PlotForTree", {
+        tree: parsed_branchset,
+        width: document.getElementById("plot").clientWidth,
+        height: .5 * document.getElementById("plot").clientWidth,
+        plot_div: "plot"
+    }));
+
     const control_div = document.getElementById("plot-controls");
     control_div.classList.add("box");
     control_div.append(htmlToElement(`
@@ -150,13 +65,18 @@ const animate = function (data) {
         document.getElementById("boottree-number").textContent = tn;
         removeChildNodes("plot");
         let pn = parseNewick(data[tn - 1][0]);
-        chart_phylogram({ parsed_data: pn, dom_id: "plot" });
+        dispatchEvent(event_build_fn("PlotForTree", {
+            tree: pn,
+            width: document.getElementById("plot").clientWidth,
+            height: .5 * document.getElementById("plot").clientWidth,
+            plot_div: "plot"
+        }));
     });
 }
 
 const hierarchy_plot_init = function (init_obj) {
     let { guid_fn, event_fn } = init_obj;
-    event_buld_fn = event_fn;
+    event_build_fn = event_fn;
     const my_guid = guid_fn();
 
     addEventListener("TreeFileContents", e => {
@@ -166,7 +86,13 @@ const hierarchy_plot_init = function (init_obj) {
                 if ("Consensus Tree" === k) {
                     let parsed_branchset = parseNewick(e.detail.contents[k][0][0]);
                     cleanExistingPlot();
-                    chart_phylogram({ parsed_data: parsed_branchset, dom_id: "plot" });
+
+                    dispatchEvent(event_build_fn("PlotForTree", {
+                        tree: parsed_branchset,
+                        width: document.getElementById("plot").clientWidth,
+                        height: .5 * document.getElementById("plot").clientWidth,
+                        plot_div: "plot"
+                    }));
                 }
                 if (RegExp(/.*boottree.*/).test(k)) { //TODO: this needs fixing.
                     animate(e.detail.contents[k]);
@@ -179,7 +105,7 @@ const hierarchy_plot_init = function (init_obj) {
     addEventListener("TreePlotRequest", e => {
         FILE_NAMES.forEach(rx => {
             if (rx.test(e.detail.file_name)) {
-                dispatchEvent(event_buld_fn("TreeFileContentsRequest", { guid: my_guid, files: [e.detail.file_id] }));
+                dispatchEvent(event_build_fn("TreeFileContentsRequest", { guid: my_guid, files: [e.detail.file_id] }));
             }
         });
     });
