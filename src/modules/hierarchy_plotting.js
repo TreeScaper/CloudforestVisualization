9fcb068aec156d7e711afadd4f601a1132af7820
mcgo0092 <mcgo0092@umn.edu>
2020-06-01 12:52:44 -0500

    refactor out duplicate newick parse code

--- src/modules/hierarchy_plotting.js
+++ src/modules/hierarchy_plotting.js
@@ -1,44 +1,9 @@
 import { removeChildNodes, cleanExistingPlot, htmlToElement } from "./html_templates";
+import { newick_parse as parseNewick } from "./tree_data_parsing"
 
 let event_build_fn = undefined;
 
 const FILE_NAMES = [RegExp(/[Cc]onsensus Tree/), RegExp(/.*boot.*tree.*/)];
-
-// https://github.com/jasondavies/newick.js
-const parseNewick = function (s) {
-    var ancestors = [];
-    var tree = {};
-    var tokens = s.split(/\s*(;|\(|\)|,|:)\s*/);
-    for (var i = 0; i < tokens.length; i++) {
-        var token = tokens[i];
-        switch (token) {
-            case '(': // new branchset
-                var subtree = {};
-                tree.branchset = [subtree];
-                ancestors.push(tree);
-                tree = subtree;
-                break;
-            case ',': // another branch
-                var subtree = {};
-                ancestors[ancestors.length - 1].branchset.push(subtree);
-                tree = subtree;
-                break;
-            case ')': // optional name next
-                tree = ancestors.pop();
-                break;
-            case ':': // optional length next
-                break;
-            default:
-                var x = tokens[i - 1];
-                if (x == ')' || x == '(' || x == ',') {
-                    tree.name = token;
-                } else if (x == ':') {
-                    tree.length = parseFloat(token);
-                }
-        }
-    }
-    return tree;
-};
 
 
 const animate = function (data) {
